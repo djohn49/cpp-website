9d5ce9dd81024cabb29a888add2bf34a0bb16ae8
David Johnson <djohn49@illinois.edu>
2019-09-23 22:45:27 -0500

    Added video descriptions

--- files/video.js
+++ files/video.js
@@ -4,11 +4,28 @@ function getVideoFile(){
     return "./files/videos/" + videoName + ".webm";
 }
 
+function getDescriptionFile(){
+    var url = new URL(window.location.href);
+    var videoName = url.searchParams.get("id");
+    return "./files/descriptions/" + videoName;
+}
+
 function onVideoLoad(){
     //update video element
     var videoElement = document.getElementById("main-video");
     var videoFile = getVideoFile();
     videoElement.setAttribute("src", videoFile);
+
+    //update description element
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("video-description").innerHTML =
+            this.responseText;
+        }
+    };
+    xhttp.open("GET", getDescriptionFile(), true);
+    xhttp.send();
 }
 
 function updateSpeed(speed){
